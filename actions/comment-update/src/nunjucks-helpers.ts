395688a0710fab5d2557c9738a5f9141bc29287d
Ludovic DEHON <tchiot.ludo@gmail.com>
2025-09-16 13:03:11 +0200

    feat: refactor the plugins workflow (#10)
    - refactor all actions / composite
    - change the java report actions
    - add trivy scan on generated jar
    - refactor the generated slack alert
    - add a new comment format that include al these

---
+++ actions/comment-update/src/nunjucks-helpers.ts
@@ -0,0 +1,27 @@
+const dateTimeFormat = new Intl.DateTimeFormat('default', {
+    month: 'short',
+    day: 'numeric',
+    year: '2-digit',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    timeZone: 'utc',
+    timeZoneName: 'short'
+})
+
+export function humanReadableSize(bytes: number) :string {
+    if (bytes === 0) {
+        return '0 Bytes'
+    }
+    const decimals = 2
+    const k = 1024
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    const i = Math.floor(Math.log(bytes) / Math.log(k))
+    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(decimals)).toString()} ${sizes[i]}`
+}
+
+export function humanReadableDate(d: string) :string {
+    const date = Date.parse(d)
+    return dateTimeFormat.format(date)
+}
+
